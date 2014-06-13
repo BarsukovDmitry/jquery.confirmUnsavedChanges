6879347858e8117658d798bf0d7e5166052af91d
BarsukovDmitry <BarsukovDmitry@outlook.com>
2014-06-13 23:56:34 +0400

    initial commit

---
+++ jquery.confirmUnsavedChanges.js
@@ -0,0 +1,31 @@
+﻿/*
+    jquery.confirmUnsavedChanges
+	http://github.com/BarsukovDmitry/jquery.confirmUnsavedChanges/
+	
+	Author: Barsukov Dmitry
+	Date: 6/2/2014
+*/
+
+(function ($) {
+
+	var unsaved = false;
+
+    $.fn.extend({
+    	confirmUnsavedChanges: function (message) {
+
+            //EVENTS
+
+            $(this).find('input,select,textarea').change(function() {
+                unsaved = true;
+            });
+
+            $(window).on('beforeunload', function () {
+                if (unsaved) {
+                	return message;
+                }
+            });
+
+        }
+    });
+
+}(window.jQuery));
